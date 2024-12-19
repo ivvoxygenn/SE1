f717ce6dee770a4a3753350678ff1e854d27cfb2
Maxim Ivanov <knifeugc@mail.ru>
2024-12-19 17:08:40 +0500

    Ввод двух чисел с приглашением

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    double num1, num2;
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    std::cout << "Вы ввели числа: " << num1 << " и " << num2 << std::endl;
     return 0;
 }
