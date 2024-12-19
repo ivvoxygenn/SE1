49a1f1f3728d08ccc67eeb927de9618c4c3f65c5
Maxim Ivanov <knifeugc@mail.ru>
2024-12-19 17:41:36 +0500

    Вывод также разности введенных чисел.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
 int main() {
-    // Объявление переменных для чисел
     double num1, num2;
     std::cout << "Введите первое число: ";
     std::cin >> num1;
     std::cout << "Введите второе число: ";
     std::cin >> num2;
     double sum = num1 + num2;
+    double difference = num1 - num2;
     std::cout << "Сумма чисел: " << sum << std::endl;
+    std::cout << "Разность чисел: " << difference << std::endl;
     return 0;
 }
